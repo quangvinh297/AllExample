b4e40ee521be2ce7cc0258bd7cedf4acf609b638
Acer <Acer@KarnikV>
2020-10-10 18:51:45 +0700

    Bài 7

---
+++ Example07/src/main/java/com/nguyenducquangvinh/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.nguyenducquangvinh.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nguyenducquangvinh.bean.Employee;
+
+@Controller
+public class EmployeeController {
+	@RequestMapping("/hello")
+	public String display(Model m) {
+		m.addAttribute("emp",new Employee());
+		return "viewpage";
+	}
+	@RequestMapping("/helloagain")
+	public String submitForm(@Valid @ModelAttribute("emp") Employee e,BindingResult br) {
+		if(br.hasErrors()) {
+			return "viewpage";
+		}
+		else {
+			return "confirmation-page";
+		}
+	}
+}
